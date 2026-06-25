9f321611c6d6e1af2d1028285b69640966fe887a
jegatheesans <jegatheesans@users.noreply.github.com>
2026-06-25 16:50:21 +0530

    Create PythonAI.py

---
+++ PythonAI.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from google import genai
+st.markdown(
+    """
+    <h1 style='text-align: center;'> Python AI Assistant </h1>
+    <p style='text-align: center; font-size:18px;'>
+        Ask any python programming question.
+    </p>
+    """,
+    unsafe_allow_html=True,
+)
+
+robo = genai.Client(api_key="MAY_APY_KEY")
+mychat = robo.chats.create(model = "gemini-flash-lite-latest")
+# Place holder for the response
+response_placeholder = st.empty()
+
+question = st.text_input("", placeholder="Enter your python question here")
+
+col1, col2, col3 = st.columns([4, 1, 4])
+with col2:
+    send=st.button("send")
+if send:
+    response= mychat.send_message(question)
+    response_placeholder.write(response.text)
