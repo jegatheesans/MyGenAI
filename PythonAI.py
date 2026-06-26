6796edc83ac19c7e8d4d9c0f9a79017ef765dab2
jegatheesans <jegatheesans@users.noreply.github.com>
2026-06-26 15:08:42 +0530

    Update PythonAI.py

--- PythonAI.py
+++ PythonAI.py
@@ -1,25 +1,31 @@
 import streamlit as st
 from google import genai
+from google.genai import types
+config = types.GenerateContentConfig(
+    system_instruction = """.You are an expert Python developer.
+ Answer only questions related to Python programming.
+ For any non-Python question, reply exactly:
+ Please ask a Python-related question.
+ Do not answer questions outside the Python domain."""
+  )
 st.markdown(
-    """
-    <h1 style='text-align: center;'> Python AI Assistant </h1>
-    <p style='text-align: center; font-size:18px;'>
-        Ask any python programming question.
-    </p>
-    """,
-    unsafe_allow_html=True,
+  """
+  <h1 style='text-align: center;'> Python AI Assistant</h1>
+  <p style='text-align: center; font-size:18px;'>
+    Ask any Python programming question.
+  </p>
+  """,
+  unsafe_allow_html=True,
 )
-
-robo = genai.Client(api_key="MY_API_KEY")
-mychat = robo.chats.create(model = "gemini-flash-lite-latest")
-# Place holder for the response
+robo = genai.Client( api_key=st.secrets["MY_API_KEY"])
+mychat = robo.chats.create(model="gemini-flash-lite-latest")
+Placeholder for the response
 response_placeholder = st.empty()
-
-question = st.text_input("", placeholder="Enter your python question here")
-
+question = st.text_input("", placeholder="Enter your Python question here...")
 col1, col2, col3 = st.columns([4, 1, 4])
 with col2:
-    send=st.button("send")
+  send =st.button("Send")
 if send:
-    response= mychat.send_message(question)
-    response_placeholder.write(response.text)
+  question = question + config.system_instruction
+  response = mychat.send_message(question)
+  response_placeholder.write(response.text)
